a44bf62f714492541719f49734f12d4d4f7d2d55
Paul <43698605+chemistrytocode@users.noreply.github.com>
2022-03-23 16:28:36 +0000

    OU - First Iteration of Chemical-Handler Service #2 (#2)
    * OU - Multihazard lookup working
    
    * OU - Fixed Swagger and tidied up

--- Controllers/HazardController.cs
+++ Controllers/HazardController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using rischy.chemical_handler.Models;
@@ -7,20 +8,42 @@ using rischy.chemical_handler.Services;
 namespace rischy.chemical_handler.Controllers
 {
     [ApiController]
-    [Route("/hazard")]
     public class HazardController : Controller
     {
         private readonly ChemicalService _chemicalService;
         public HazardController(ChemicalService chemicalService) => _chemicalService = chemicalService;
         
-        [HttpGet(Name = "GetAllHazards")]
+        [Route("/all-hazards")]
+        [HttpGet]
         public async Task<List<ChemicalHazard>> GetHazards() => await _chemicalService.GetHazardAsync();
-
         
-        [HttpPost(Name = "AddChemicals")]
-        public string Post()
+        [Route("/hazards")]
+        [HttpPost]
+        public string PostHazards()
         {
             return "Adding Chemical data";
         }
+        
+        [Route("/hazards")]
+        [HttpGet]
+        public async Task<IEnumerable<ChemicalHazard?>> GetHazardsById(string ids)
+        {
+            var listOfIds = ids.Split(",").ToList();
+            return await FetchChemicalHazards(listOfIds);
+        }
+        
+        private async Task<IEnumerable<ChemicalHazard?>> FetchChemicalHazards(IEnumerable<string> ids)
+        {
+            var getAllChemicalsFromIds = await Task.Run(() => 
+                ids.Select(async id => await FetchChemicalFromId(id))
+                    .ToList());
+       
+            await Task.WhenAll(getAllChemicalsFromIds);
+            var filteredChemicalTaskResults = getAllChemicalsFromIds.Select(task => task.Result);
+        
+            return filteredChemicalTaskResults;
+        }
+        
+        private async Task<ChemicalHazard?> FetchChemicalFromId(string id) => await _chemicalService.GetHazardAsync(id);
     }
 }
---
+++ Models/Chemicals/EmergencyActions.cs
@@ -0,0 +1,8 @@
+namespace rischy.chemical_handler.Models
+{
+    public record EmergencyActions
+    {
+        public string? Emergency { get; set; }
+        public string? Action { get; set; }
+    }
+}
