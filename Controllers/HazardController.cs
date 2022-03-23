8727784d99c719ad33bb00a83181c2f88858890a
Paul Ayling <paul.ayling@bglgroup.co.uk>
2022-03-23 16:48:09 +0000

    OU - CRUD methods on Stock & Hazard controllers

--- Controllers/HazardController.cs
+++ Controllers/HazardController.cs
@@ -8,42 +8,39 @@ using rischy.chemical_handler.Services;
 namespace rischy.chemical_handler.Controllers
 {
     [ApiController]
+    [Route("/hazards")]
     public class HazardController : Controller
     {
         private readonly ChemicalService _chemicalService;
         public HazardController(ChemicalService chemicalService) => _chemicalService = chemicalService;
-        
-        [Route("/all-hazards")]
-        [HttpGet]
-        public async Task<List<ChemicalHazard>> GetHazards() => await _chemicalService.GetHazardAsync();
-        
-        [Route("/hazards")]
-        [HttpPost]
-        public string PostHazards()
-        {
-            return "Adding Chemical data";
-        }
-        
-        [Route("/hazards")]
-        [HttpGet]
-        public async Task<IEnumerable<ChemicalHazard?>> GetHazardsById(string ids)
-        {
-            var listOfIds = ids.Split(",").ToList();
-            return await FetchChemicalHazards(listOfIds);
-        }
-        
+
         private async Task<IEnumerable<ChemicalHazard?>> FetchChemicalHazards(IEnumerable<string> ids)
         {
             var getAllChemicalsFromIds = await Task.Run(() => 
                 ids.Select(async id => await FetchChemicalFromId(id))
                     .ToList());
-       
+
             await Task.WhenAll(getAllChemicalsFromIds);
             var filteredChemicalTaskResults = getAllChemicalsFromIds.Select(task => task.Result);
         
             return filteredChemicalTaskResults;
         }
-        
+    
         private async Task<ChemicalHazard?> FetchChemicalFromId(string id) => await _chemicalService.GetHazardAsync(id);
+        
+        [HttpGet(Name="GetHazardsById")]
+        // Example: /hazards?id=123,456,789
+        public async Task<IEnumerable<ChemicalHazard?>> GetHazardsById(string ids)
+        {
+            var listOfIds = ids.Split(",").ToList();
+            return await FetchChemicalHazards(listOfIds);
+        }
+
+        [HttpPost(Name="AddHazard")]
+        public async Task PostStock(ChemicalHazard chemicalHazard) => await _chemicalService.CreateHazardAsync(chemicalHazard);
+        
+        [Route("/all-hazards")]
+        [HttpGet(Name="GetAllHazards")]
+        public async Task<List<ChemicalHazard>> GetAllHazards() => await _chemicalService.GetHazardAsync();
     }
 }
--- Models/Chemicals/ChemicalStock.cs
+++ Models/Chemicals/ChemicalStock.cs
@@ -3,9 +3,8 @@ namespace rischy.chemical_handler.Models
     public record ChemicalStock : Chemical
     {
         public int? StockLevel { get; set; }
-
         public int? MaxHeld { get; set; }
-
         public bool ConsultationRequired { get; set; } = false;
+        public string Hazard_Id { get; set; }
     }
 }
